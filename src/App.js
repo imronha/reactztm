008eee54720dbc00adc72751db31f4fcb6d60d95
imronha <imronha@gmail.com>
2023-11-15 20:11:08 -0800

    added componentDidMount and monster api

--- src/App.js
+++ src/App.js
@@ -6,27 +6,33 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      monsters: [
-        {
-          name: 'Mikan',
-        },
-        {
-          name: 'Yuki',
-        },
-        {
-          name: 'Brownie'
-        }
-      ]
+      monsters: [],
     }
   }
 
+  // This code will run when React renders the component
+  // The only time a component will "remount" is if it was unmounted
+  componentDidMount(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(users => 
+        this.setState(
+          () => {
+            return {monsters: users};
+          },
+          () => {
+            console.log(this.state);
+          }
+        )
+      )
+  }
 
   render() {
     return (
       <div className="App">
         {
           this.state.monsters.map((monster) => {
-            return <h1>{monster.name}</h1>
+            return <h1 key={monster.id}>{monster.name}</h1>
           })
         }
 
